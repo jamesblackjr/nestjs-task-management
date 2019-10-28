76d0b0aa250d7d91662ed51fa924d13cac1869a4
James A. Black Jr <mrdesigner@jamesblackjr.com>
2019-10-27 19:12:31 -0500

    Add JWT Strategy with Passport to Auth Module.

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,8 @@
 import * as bcrypt from 'bcrypt';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
