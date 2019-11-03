432ebc3cc0aa390c055673a0a279871c6196ec0a
James A. Black Jr <mrdesigner@jamesblackjr.com>
2019-11-03 09:37:00 -0600

    Replace bcrypt with bcryptjs package.

--- src/auth/user.repository.spec.ts
+++ src/auth/user.repository.spec.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Test } from '@nestjs/testing';
 import { UserRepository } from './user.repository';
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
