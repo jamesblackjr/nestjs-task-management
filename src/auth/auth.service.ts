abca4b6b6ddfab3a7f85218a9950935cb9f3223c
James A. Black Jr <mrdesigner@jamesblackjr.com>
2019-11-03 11:21:20 -0600

    Remove Debug Log for JWT Access Token.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
     const payload: JwtPayload = { username };
     const accessToken = await this.jwtService.sign(payload);
 
-    this.logger.debug(`Generated JWT Access Token with payload: ${JSON.stringify(payload)}`); // TODO: Probably shouldn't log payload.
+    // this.logger.debug(`Generated JWT Access Token with payload: ${JSON.stringify(payload)}`); // TODO: Probably shouldn't log payload.
 
     return { accessToken };
   }
